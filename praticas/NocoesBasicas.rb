40e4f96dc5969d95d6781c98b8e83d6308502e69
Geisianny Bernardo <geisiannybernardo@gmail.com>
2022-07-01 20:44:27 -0300

    praticas

---
+++ praticas/NocoesBasicas.rb
@@ -0,0 +1,71 @@
+#puts e print -> saida
+
+#print ("Digite seu nome: ")
+
+#entrada 
+#nome = gets.chomp
+
+#print("Digite seu sobrenome: ")
+#sobrenome = gets.chomp
+
+#puts("Hello #{nome} #{sobrenome}!")
+
+=begin
+
+puts("Digite dois numero: ")
+num1 = gets.to_f
+num2 = gets.to_f
+
+adicao = num1 + num2
+
+puts("O resultado da soma é: #{adicao}")
+num1 = gets.to_f
+num2 = gets.to_f
+
+adicao = num1 + num2
+
+puts("O resultado da soma é: #{adicao}")
+
+
+        *** EXERCICIO 1 ***
+puts("Digite o nome:  ")
+nome = gets.chomp
+
+puts("Digite o idade: ")
+idade = gets.to_i
+
+puts("Nome: #{nome}\nIdade: #{idade}")
+
+
+                 *** EXERCICIO 2 ***
+puts("Digite dois numeros:  ")
+num1 = gets.to_i
+num2 = gets.to_i
+
+soma = num1 + num2
+
+subtracao = num1 - num2
+
+mult = num1 * num2
+
+div = num1 / num2
+
+puts("Soma: #{soma}\nSubtração: #{subtracao}\nMulti: #{mult}\nDivisao: #{div}")
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
